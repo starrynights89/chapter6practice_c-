fabf3c47cc69fe362dc2b59322aa736c93c2ee0c
starrynights89 <alexander-hartson@live.com>
2017-11-07 14:58:18 -0600

    code changes

--- calculator.cpp
+++ calculator.cpp
@@ -4,33 +4,9 @@ int main()
 {
     cout << "Please enter expression (we can handle +,-,*, and /)\n";
     cout << "add an x to end expression (e.g., 1+2*3x): ";
-    int lval = 0;
-    int rval;
-    cin >> lval; //read left most operand
-    if(!cin) error("no first operand");
-    for (char op; cin >> op; ) //read operator and right-hand operand repeatedly
+    double expression()
     {
-        if (op!='x') cin >> rval;
-        if (!cin) error("no second operand");
-        switch(op)
-        {
-            case '+':
-                lval += rval; //add: lval = lval + rval
-                break;
-            case '-':
-                lval -= rval; //subtract: lval = lval - rval
-                break; 
-            case '*':
-                lval *= rval; //multiply: lval = lval * rval
-                break;
-            case '/':
-                lval /= rval; //divide: lval = lval / rval
-                break;
-            default:          //not another operator: print result
-                cout << "Result: " << lval << '\n';
-                keep_window_open();
-                return 0;
-        }
+        double left = term();         //read and evalute a Term
+        Token t = get_token();        //get the next token
     }
-    error("bad expression"); 
 }
