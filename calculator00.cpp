bccc332c5d6324291339640ce36d72ce23874ec0
starrynights89 <alexander-hartson@live.com>
2017-11-07 17:51:23 -0600

    adding code

--- calculator00.cpp
+++ calculator00.cpp
@@ -3,6 +3,16 @@
 double expression()
 {
     double left = expression();  //read and evaluate an Expression
+    Token t = get_token();       //get the next token
+    switch(t.kind)               //see which kind of token it is
+    {
+        case '+':
+            return left + term(); //read and evalute a Term then do an add
+        case '-':
+            return left - term(); //read and evalute a Term then do a subtraction
+        default:
+            return left;          //return the value of the Expression
+    }
 }
 
 int main()
