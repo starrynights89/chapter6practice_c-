2f44a83ddba19bba5729c213bfc192f9db3c42ee
starrynights89 <alexander-hartson@live.com>
2017-11-08 01:00:23 -0600

    adding function

--- calculator00.cpp
+++ calculator00.cpp
@@ -41,6 +41,28 @@ catch (...)
     return 2; 
 }
 
+double expression()
+{
+    double left = term(); //read and evaluate a Term
+    Token t = get_token(); //get the next token
+    while (true)
+    {
+        switch (t.kind)
+        {
+            case '+':
+                left += term(); //evaluate a Term and add
+                t = get_token();
+                break;
+            case '-':
+                left -= term(); //evaluate a Term and subtract
+                t = get_token();
+                break;
+            default:
+                return left; //finally: no more + or -; return the answer
+        }
+    }
+}
+
 double term()
 {
     double left = primary(); //read and evaluate a Term
