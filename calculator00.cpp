fabb780028f6554e279ad6cd2c27a5aa8bc56a31
Alexander Hartson <alexander-hartson@live.com>
2017-11-08 15:37:02 -0600

    Add files via upload

--- calculator00.cpp
+++ calculator00.cpp
@@ -5,6 +5,10 @@ class Token
 public:
     char kind; //what kind of token
     double value; //for numbers: a value
+	Token(char ch) //make a Token from char
+		:kind(ch), value(0) {}
+	Token(char ch, double val) //make a Token from a char and a double
+		:kind(ch), value(val) {}
 };
 
 Token get_token()
