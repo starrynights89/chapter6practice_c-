2a82f24d7771e1199b49566ed5e184f13ec0af3e
starrynights89 <alexander-hartson@live.com>
2017-11-07 23:20:39 -0600

    adding code

--- calculator00.cpp
+++ calculator00.cpp
@@ -40,3 +40,19 @@ catch (...)
     keep_window_open();
     return 2; 
 }
+
+double expression()
+{
+    double left = expression(); //read and evaluate an Expression
+    Token t = get_token();      //get the next token
+    switch(t.kind)              //see which kind of token it is
+    {
+        case '+':
+            return left + term(); //read and evaluate a Term then do an add
+        case '-':
+            return left - term(); //read and evaluate a Term then do a subtraction
+        
+        default:
+            return left;          //return the value of the Expression
+    }
+}
