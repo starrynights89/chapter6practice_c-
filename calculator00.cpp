cedf316bff8674d89f66a4294e440580ea96a80c
starrynights89 <alexander-hartson@live.com>
2017-11-08 02:00:29 -0600

    bug fixing

--- calculator00.cpp
+++ calculator00.cpp
@@ -45,24 +45,25 @@ catch (...)
     return 2; 
 }
 
-double primary()
+double expression()
 {
-	Token t = get_token();
-	switch (t.kind)
+	double left = term(); //read and evaluate a Term
+	Token t = get_token(); //get the next token
+	while (true)
 	{
-	case'(': //handle '('expression')'
-	{
-		double d = expression();
-		t = get_token();
-		if (t.kind != ')') error("')' expected");
+		switch (t.kind)
 		{
-			return d;
+		case '+':
+			left += term(); //evaluate a Term and add
+			t = get_token();
+			break;
+		case '-':
+			left -= term(); //evaluate a Term and subtract
+			t = get_token();
+			break;
+		default:
+			return left; //finally: no more + or -; return the answer
 		}
-	}
-	case '8': //we use '8' to represent a number
-		return t.value; //return the number's value
-	default:
-		error("primary expected");
 	}
 }
 
@@ -92,24 +93,23 @@ double term()
 	}
 }
 
-double expression()
+double primary()
 {
-	double left = term(); //read and evaluate a Term
-	Token t = get_token(); //get the next token
-	while (true)
+	Token t = get_token();
+	switch (t.kind)
 	{
-		switch (t.kind)
+	case'(': //handle '('expression')'
+	{
+		double d = expression();
+		t = get_token();
+		if (t.kind != ')') error("')' expected");
 		{
-		case '+':
-			left += term(); //evaluate a Term and add
-			t = get_token();
-			break;
-		case '-':
-			left -= term(); //evaluate a Term and subtract
-			t = get_token();
-			break;
-		default:
-			return left; //finally: no more + or -; return the answer
+			return d;
 		}
+	}
+	case '8': //we use '8' to represent a number
+		return t.value; //return the number's value
+	default:
+		error("primary expected");
 	}
 }
