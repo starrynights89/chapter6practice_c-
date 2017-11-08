ae082fe0eee777fe8952ddb6685bb1ff7dc787cd
starrynights89 <alexander-hartson@live.com>
2017-11-07 19:48:35 -0600

    code fix

--- calculator00.cpp
+++ calculator00.cpp
@@ -7,41 +7,11 @@ public:
     double value; //for numbers: a value
 };
 
-class Token_stream
-{
-public:
-    Token_stream();
-    Token get();
-    void putback(Token t);
-private:
-    bool full;
-    Token buffer;
-};
-
-Token_stream ts;
-
-double term(); 
-
-double expression()
-{     
-    double left = expression();        //evaluate an expression
-    Token t = ts.get();          //get the next token
-    switch(t.kind)               //see which kind of token it is
-    {
-        case '+':
-            return left + term(); //read and evalute a Term then do an add
-        case '-':
-            return left - term(); //read and evalute a Term then do a subtraction
-        default:
-            return left;          //return the value of the Expression
-    }
-}
-
 int main()
 try
 {
-    while (cin)
-        cout << expression() << '\n';
+    Token t1{'+'};
+    Token t2{'8',11.5};
     keep_window_open();
 }
 catch(exception& e)
