22ebad69d64dc4af300af6f183519f902db2bb4b
starrynights89 <alexander-hartson@live.com>
2017-11-08 01:23:59 -0600

    adding function

--- calculator00.cpp
+++ calculator00.cpp
@@ -88,3 +88,24 @@ double term()
         }
     }
 }
+
+double primary()
+{
+    Token t = get_token();
+    switch(t.kind)
+    {
+        case'(': //handle '('expression')'
+        {
+            double d = expression();
+            t = get_token();
+            if (t.kind != ')') error("')' expected");
+            {
+                return d;
+            }
+        }
+        case '8': //we use '8' to represent a number
+            return t.value; //return the number's value
+        default:
+            error("primary expected");
+    }
+}
